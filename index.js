d9d8defc1dad097fd3a807023ceea5499e478638
The Kicker <cav.lemasters@geneva.edu>
2022-09-24 17:40:23 -0400

    Base project added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*
     Node JS Web Scraper    
-    Uses Express and Cheerio
+    Uses Axios, Express and Cheerio
 
 
 */
@@ -10,5 +10,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-console.log("Server is running... ")
+const url = "https://usadinnerwaredirect.com/collections/the-fiesta-vault"
+
+axios(url)
+    .then((result) => {
+        console.log(`URL: ${url}`)
+        console.log(`STATUS: ${result.status} - ${result.statusText}`)
+        const header = result.headers
+        const html = result.data
+        const c = cheerio.load(html)
+        c('.product__title', html).each(function() {
+            const title = c(this).text()
+            console.log(title)
+        })
+        console.log(`HEADER: ${JSON.stringify(header)}`)
+    }).catch((err) => {
+        console.log(err)
+    });
+
+app.listen(port, ()=>{
+    console.log(`Server is running... port:${port}`)
+})
 
